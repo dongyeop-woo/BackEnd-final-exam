17fec96e5ee62f0a744b7ca526f2496d62280ea4
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-16 14:26:45 +0900

    feat: 파일추가

--- src/main/java/com/book/book/dto/UserRegisterRequest.java
+++ src/main/java/com/book/book/dto/UserRegisterRequest.java
@@ -1,4 +1,32 @@
 package com.book.book.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UserRegisterRequest {
+
+    private String username;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Builder
+    public UserRegisterRequest(String username, String password, String confirmPassword, String email, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
