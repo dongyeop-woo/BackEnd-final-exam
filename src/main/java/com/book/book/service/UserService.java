6f95a0e262287d87b6dfc0cd6ea9e38dbfdea28e
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:09:40 +0900

    refactor: 찜 목록 필요한 Repository 필드 추가

--- src/main/java/com/book/book/service/UserService.java
+++ src/main/java/com/book/book/service/UserService.java
@@ -1,8 +1,13 @@
 package com.book.book.service;
 
+import com.book.book.dto.UserRegisterRequestDTO;
+import com.book.book.entity.Book;
 import com.book.book.entity.User;
+import java.util.List;
 
 public interface UserService {
-    User register(User user);
+    User register(UserRegisterRequestDTO userRegisterRequestDTO);
     User login(String username, String password);
+    List<Book> getFavorites(String username);
+    void toggleFavorite(String username, Long bookId);
 }
