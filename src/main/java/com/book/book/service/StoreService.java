704c6dd5f211cf3eac80c703aa5682237601620a
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:04:35 +0900

    refactor: 불 필요한 줄 바꿈 삭제

--- src/main/java/com/book/book/service/StoreService.java
+++ src/main/java/com/book/book/service/StoreService.java
@@ -1,19 +1,16 @@
 package com.book.book.service;
 
+import com.book.book.dto.BookDTO;
 import com.book.book.entity.Book;
 
 import java.util.List;
 
 public interface StoreService {
     List<Book> findAll();
-
-    Book setBook(Book book);
-    
+    Book setBook(BookDTO bookDTO);
     List<Book> findBestsellerBooks();
-    
     List<String> findAllCategories();
-    
     List<Book> findBooksByCategory(String category);
-    
     List<Book> searchBooks(String keyword);
+    Book findById(Long id);
 }
