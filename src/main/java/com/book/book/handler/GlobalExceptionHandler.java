1010fc4447fdeb4b60f890f7e4c849e4df71d8f1
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:01:19 +0900

    refactor: 주석 추가

--- src/main/java/com/book/book/handler/GlobalExceptionHandler.java
+++ src/main/java/com/book/book/handler/GlobalExceptionHandler.java
@@ -10,11 +10,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 로그인 실패 및 유저 조회 실패 에러
     @ExceptionHandler({LoginFailException.class, UserNotFoundException.class})
     public ResponseEntity<?> handleApiException(RuntimeException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
+    // View Error 렌더링
     @ExceptionHandler(RuntimeException.class)
     public String handleViewException(RuntimeException e, Model model) {
         model.addAttribute("errorMessage", e.getMessage());
