5cd4a7b492fbef0f3931495cfb782f670dc6bf95
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:03:31 +0900

    refactor: Entity 와 DTO 변환 로직 추가

--- src/main/java/com/book/book/controller/StoreController.java
+++ src/main/java/com/book/book/controller/StoreController.java
@@ -1,6 +1,8 @@
 package com.book.book.controller;
 
+import com.book.book.dto.BookDTO;
 import com.book.book.entity.Book;
+import com.book.book.mapper.BookMapper;
 import com.book.book.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,17 @@ import java.util.List;
 public class StoreController {
 
     private final StoreService storeService;
+    private final BookMapper bookMapper;
 
+    // 책 생성
     @PostMapping
-    public ResponseEntity<Book> create(@RequestBody Book book) {
-        return ResponseEntity.ok(storeService.setBook(book));
+    public ResponseEntity<BookDTO> create(@RequestBody BookDTO bookDTO) {
+        Book newBook = storeService.setBook(bookDTO);
+        BookDTO responseDTO = bookMapper.toDTO(newBook);
+        return ResponseEntity.ok(responseDTO);
     }
 
+    // 책 조회
     @GetMapping
     public ResponseEntity<List<Book>> findAll() {
         return ResponseEntity.ok(storeService.findAll());
