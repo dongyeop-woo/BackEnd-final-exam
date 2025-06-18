f02c48e189b6cf04ab8df0354502edea5e221a90
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:08:25 +0900

    refactor: 예외 메세지를 직접 명시

--- src/main/java/com/book/book/exception/UserNotFoundException.java
+++ src/main/java/com/book/book/exception/UserNotFoundException.java
@@ -1,7 +1,5 @@
 package com.book.book.exception;
 
 public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(String message) {
-    super(message);
-  }
+    public UserNotFoundException() { super("사용자를 찾을 수 없습니다."); }
 }
