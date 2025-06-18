687ae33a4af02d0a33b6dba047d22b26d8977fa3
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:10:36 +0900

    refactor: 주석 추가 및 찜 목록 페이지 추가

--- src/main/java/com/book/book/controller/ViewController.java
+++ src/main/java/com/book/book/controller/ViewController.java
@@ -1,8 +1,12 @@
 package com.book.book.controller;
 
+import com.book.book.exception.UserNotFoundException;
 import com.book.book.service.StoreService;
+import com.book.book.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +17,15 @@ import org.springframework.web.bind.annotation.*;
 public class ViewController {
 
     private final StoreService storeService;
+    private final UserService userService;
 
+    // 메인페이지
     @GetMapping("/")
     public String redirectToMain() {
         return "redirect:/main";
     }
 
+    // 타임리프 전달
     @GetMapping("/main")
     public String mainPage(Model model) {
         addCommonAttributes(model);
@@ -27,6 +34,7 @@ public class ViewController {
         return "MainPage";
     }
 
+    // 베스트셀러 전달
     @GetMapping("/books/bestseller")
     public String bestsellerPage(Model model) {
         addCommonAttributes(model);
@@ -34,6 +42,7 @@ public class ViewController {
         return "BestsellerPage";
     }
 
+    // 카테고리별 전달
     @GetMapping("/books/category")
     public String categoryListPage(Model model) {
         addCommonAttributes(model);
@@ -41,6 +50,7 @@ public class ViewController {
         return "CategoryPage";
     }
 
+    // 카테고리별 하나하나 확인 (html에 종목별 버튼)
     @GetMapping("/books/category/{category}")
     public String categoryPage(@PathVariable String category, Model model) {
         addCommonAttributes(model);
@@ -49,6 +59,7 @@ public class ViewController {
         return "CategoryPage";
     }
 
+    // 책 검색
     @GetMapping("/books/search")
     public String searchBooks(@RequestParam(required = false) String keyword, Model model) {
         addCommonAttributes(model);
@@ -59,22 +70,35 @@ public class ViewController {
         return "SearchPage";
     }
 
-    @GetMapping("/books/{id}")
-    public String bookDetail(@PathVariable Long id, Model model) {
-        addCommonAttributes(model);
-        return "BookDetailPage";
-    }
-
+    // 로그인 페이지 이동
     @GetMapping("/login")
     public String loginPage() {
         return "LoginPage";
     }
 
+    // 회원가입 페이지 이동
     @GetMapping("/register")
     public String registerPage() {
         return "RegisterPage";
     }
 
+    // 찜 목록 페이지 이동
+    @GetMapping("/favorites")
+    public String favoritesPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+
+        // 간단 로직이라 책임분리 안함 그냥 컨트롤러에서 처리
+        if (userDetails == null) {
+            return "redirect:/login";
+        }
+        try {
+            model.addAttribute("favorites", userService.getFavorites(userDetails.getUsername()));
+        } catch (UserNotFoundException e) {
+            return "redirect:/login";
+        }
+        return "bookFavorites";
+    }
+
+    // 모든 카테고리 전달
     private void addCommonAttributes(Model model) {
         model.addAttribute("categories", storeService.findAllCategories());
     }
