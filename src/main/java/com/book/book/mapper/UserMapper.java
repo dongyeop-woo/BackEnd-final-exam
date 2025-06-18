a7c60b5d22ee4d38a7a158741e287c17f92c3b0a
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 16:08:06 +0900

    refactor: Entity와 DTO 변환 Builder 로직 추가

--- src/main/java/com/book/book/mapper/UserMapper.java
+++ src/main/java/com/book/book/mapper/UserMapper.java
@@ -1,4 +1,39 @@
 package com.book.book.mapper;
 
+import com.book.book.dto.UserRegisterRequestDTO;
+import com.book.book.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+
+@Component
+@RequiredArgsConstructor
 public class UserMapper {
+
+    public User toEntity(UserRegisterRequestDTO request) {
+        return User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .build();
+    }
+
+    public UserRegisterRequestDTO toDTO(User user) {
+        return UserRegisterRequestDTO.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+    }
+
+    public UserDetails toSecurityUser(User user) {
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(new ArrayList<>())
+                .build();
+    }
 }
