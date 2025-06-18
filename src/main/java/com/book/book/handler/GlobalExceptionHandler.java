b4427246937d05bd73a7dc876ae2116fe48e37f9
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 11:04:52 +0900

    feat: Handler 코드 생성

--- src/main/java/com/book/book/handler/GlobalExceptionHandler.java
+++ src/main/java/com/book/book/handler/GlobalExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.book.book.handler;
 
+import com.book.book.exception.LoginFailException;
+import com.book.book.exception.UserNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({LoginFailException.class, UserNotFoundException.class})
+    public ResponseEntity<?> handleApiException(RuntimeException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleViewException(RuntimeException e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error";
+    }
 }
