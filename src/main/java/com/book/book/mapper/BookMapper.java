628a46652cba6acb112545520f09a8a09a5c95c0
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-18 11:04:23 +0900

    refactor: UserMapper로 잘못된 코드 BookMapper로 수정

--- src/main/java/com/book/book/mapper/BookMapper.java
+++ src/main/java/com/book/book/mapper/BookMapper.java
@@ -1,45 +1,27 @@
 package com.book.book.mapper;
 
-import com.book.book.dto.UserRegisterRequest;
-import com.book.book.entity.User;
+import com.book.book.dto.BookDTO;
+import com.book.book.entity.Book;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import java.util.ArrayList;
-
 
 @Component
 @RequiredArgsConstructor
-public class UserMapper {
+public class BookMapper {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public User toUser(User user) {
-        return User.builder()
-                .username(user.getUsername())
-                .password(passwordEncoder.encode(user.getPassword()))
-                .email(user.getEmail())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
+    public Book toEntity(BookDTO dto) {
+        return Book.builder()
+                .name(dto.getName())
+                .author(dto.getAuthor())
+                .category(dto.getCategory())
                 .build();
     }
 
-    public UserDetails toSecurityUser(User user) {
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .authorities(new ArrayList<>())
-                .build();
-    }
-
-    public User registerUser(UserRegisterRequest request) {
-        return User.builder()
-                .username(request.getUsername())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .email(request.getEmail())
-                .firstName(request.getFirstName())
-                .lastName(request.getLastName())
+    public BookDTO toDTO(Book book) {
+        return BookDTO.builder()
+                .name(book.getName())
+                .author(book.getAuthor())
+                .category(book.getCategory())
                 .build();
     }
 }
